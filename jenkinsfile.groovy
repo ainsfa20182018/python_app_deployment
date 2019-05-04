64887a4ad76198fa4be96ad2b7d400c998f76d9d
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-05-04 14:28:57 -0500

    python

---
+++ jenkinsfile.groovy
@@ -0,0 +1,13 @@
+node {
+    stage("Clone repo"){
+        git "https://github.com/ainsfa20182018/flask-examples.git"
+    }
+    stage("Install Reuirments"){
+        sh "scp -r *  ec2-user@${ENV}:/tmp"
+        sh "ssh ec2-user@${ENV} virtualenv  /tmp/venv"
+        sh "ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+    }
+    stage("Start python app"){
+        sh "ec2-user@${ENV} python /tmp/01-hello-world/hello.py"
+    }
+}
